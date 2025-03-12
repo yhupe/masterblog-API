8a1af83c8c14b9b21057a2556992bd642023e30b
yhupe <hannes.pickel@outlook.com>
2025-03-12 21:44:49 +0100

    adding endpoint /api/posts to add new blog post via POST request

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,6 +13,25 @@ POSTS = [
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    new_post = request.get_json()
+
+    if "title" not in new_post or len(new_post['title']) == 0:
+        return jsonify({"error": "key 'title' must exist / can't be empty"}), 400
+
+    elif "content" not in new_post or len(new_post['content']) == 0:
+        return jsonify({"error": "key 'content' must exist / can't be empty"}), 400
+
+    else:
+        new_id = max(post['id'] for post in POSTS) + 1
+        new_post['id'] = new_id
+
+        POSTS.append(new_post)
+        print(f"Post with ID {new_id} added successfully.")
+        return jsonify(new_post), 201
 
 
 if __name__ == '__main__':
